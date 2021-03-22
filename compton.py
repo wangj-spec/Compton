981b82976d3d030023304658dd4bb60e7abd2335
wangj-spec <77452051+wangj-spec@users.noreply.github.com>
2021-03-22 00:11:17 +0000

    Update compton.py

--- compton.py
+++ compton.py
@@ -1,221 +1,273 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Mar 11 16:15:01 2021
-
-@author: leonardobossi1
-"""
 
 import numpy as np
-import matplotlib as mpl
-from scipy.optimize import curve_fit
+import random as rnd
+from scipy.stats import norm
 import matplotlib.pyplot as plt
 
-def linear (x, a, b):
-    y = a*x + b
-    return y
+re = 2.8179 * 10 ** -15
 
-def csenergy(angle, e0): 
-    energymeasure = e0 / ( 1 + (e0 / 511) * (1 - np.cos(np.deg2rad(angle))))
-    
-    return energymeasure
 
-def errorprop(a, b, siga, sigb, value):
-    asserror = value * np.sqrt(siga / a ** 2 + sigb/ b ** 2)
+def photon_E(E_initial, theta, e_rest=511):
+    E_final = E_initial / (1 + (E_initial * (1 - np.cos(theta)) / e_rest))
 
-    return asserror
+    return E_final
 
-data = np.loadtxt("Experiment_Data.csv", delimiter = ",", skiprows= 7, unpack = True )
-data2 = np.genfromtxt("Experiment_Data.csv", delimiter = ",", max_rows= 7,skip_header=1, dtype="str")
 
-# Reading the data (from provided data ile)
-channel,nosource,na22calib,mn54calib,cs137calib,am241calib,c20cs137,b20cs137,c30cs137,b30cs137,c45cs137,b45cs137 = data
+def Gaussian(x, const, sigma, u):
+    '''
+    Returns Guassian function for a given x value and Gaussian parameters.
+    sigma::float
+        Standard deviation
+    const::float
+        constant
+    u::float
+        mean value
+    '''
+    return (const / (sigma * np.sqrt(2 * np.pi))) * np.e ** (-0.5 * ((x - u) / sigma) ** 2)
 
-# Removing the background noise
-na22calib -= nosource
-mn54calib -= nosource
-cs137calib -= nosource
-am241calib -= nosource
 
-# Energy peaks for the different radioactive sources
-na22e = [511.006, 1274.542]
-mn54e = 834.838
-cs137e = [33, 661.657]
-am241e = 59
+def analogsignal(angle, e0, gain, e_energy=511):
+    # Calculating the scattering energy from the Compton relation
+    energymeasure = e0 / (1 + (e0 / e_energy) * (1 - np.cos(np.deg2rad(angle))))
 
-nachann1 = np.argmax(na22calib)
-nachann2 = np.argmax(na22calib[200:]) + 200
-mnchann = np.argmax(mn54calib)
-cschann1 = np.argmax(cs137calib)
-cschann2 = np.argmax(cs137calib[:100])
-amchann = np.argmax(am241calib)
+    # Computing the analog signal by multiplying the gain.
+    analogsignal = energymeasure * gain
 
-channels = [nachann1, nachann2, mnchann, cschann1,cschann2,amchann]
-energies = [na22e[0], na22e[1], mn54e, cs137e[1], cs137e[0], am241e]
+    return analogsignal
 
-popt, pcov = curve_fit(linear, channels, energies)
 
-yvals = list(map(lambda x: linear(x, *popt), channel))
+def theta_detector(r, diam=50.8e-3):
+    '''
+    Returns angluar range of the detector.
+    '''
 
-# Calibrating the channels to energy values using the peaks 
-plt.plot(channel, yvals, label = str(popt[0]) + "x + " + str(popt[1]))
-plt.scatter(channels, energies)
-plt.title("Calibration of Energy against Channel")
-plt.ylabel("Energy (keV)")
-plt.xlabel("Channel")
-plt.grid()
-plt.legend()
+    dtheta = 2 * np.arcsin(diam / (2 * r))
 
-plt.figure()
-plt.scatter(channel, na22calib)
-plt.title("Spectrum of Na-22")
-plt.xlabel('Bin no.')
-plt.ylabel('Total counts')
-plt.grid()
+    return dtheta
 
-plt.figure()
-plt.scatter(channel, mn54calib)
-plt.title("Spectrum of Mn-54")
-plt.xlabel('Bin no.')
-plt.ylabel('Total counts')
-plt.grid()
 
-plt.figure()
-plt.scatter(channel, cs137calib)
-plt.title("Spectrum of Cs-137")
-plt.xlabel('Bin no.')
-plt.ylabel('Total counts')
-plt.grid()
+def backscatter(energy, gain):
+    seed = rnd.random()
+    angle = norm.ppf(seed, loc=180, scale=0.05)  # angle choosing in degrees
+    signalmeasured = analogsignal(angle, energy, gain)
 
-plt.figure()
-plt.scatter(channel, am241calib)
-plt.title("Spectrum of Am-241")
-plt.xlabel('Bin no.')
-plt.ylabel('Total counts')
-plt.grid()
+    return signalmeasured
 
+
+def cross_diff(theta, E_initial, re=re):
+    E_scatter = photon_E(E_initial, theta)
+    P = E_scatter / E_initial
+
+    cross_diff = 1 / 2 * (re ** 2) * (P ** 2) * (P + 1 / P - (np.sin(theta)) ** 2)
+
+    return cross_diff
+
+def linear_interpolation(array1, array2, x):
+    for i in range(len(array1) - 1):
+        current = array1[i]
+        next = array1[i+1]
+        if next > x:
+            value = array2[i] + (array2[i+1] - array2[i]) *(x - current)/(next - current)
+        else:
+            continue
+    if x >= array1[-1]:
+        value = array2[-1] + (array2[-1] - array2[-2])*(x - array1[-1])/(array1[-1]-array1[-2])
+
+    return value
+
+
+def generate_noise(N, analogsig, gain, angledist, det_res=0.075, max_signal=5, bit_depth=9, probdect=1, relprob=0):
+    bins = {}
+
+    for i in range(2 ** bit_depth):
+        bins[i] = 0
+
+    for i in range(N):
+
+        detectionprob = rnd.random()
+        probscatt_n = rnd.random()
+
+        seed = rnd.random()
+        noise_signal = norm.ppf(seed, loc=0, scale=det_res * max_signal / 2)  # Gaussian noise
+
+        if detectionprob <= probdect:   #probability of normal absorption within detector
+
+
+            tot_signal = analogsig + noise_signal
+
+            bin_val = np.floor((2 ** bit_depth) * tot_signal / max_signal)
+
+            bins[bin_val] += 1
+
+        elif detectionprob > probdect:
+
+            if probscatt_n <= relprob: # compton scattering within detector
+                seed = rnd.random()
+
+                angles = angledist[0]
+                probability = angledist[1]
+                angle = linear_interpolation(probability, angles,seed)
+
+                energy = analogsig / gain
+                scattered = analogsignal(angle, energy, gain)
+                signal = analogsig - scattered
+                tot_signal = signal + noise_signal
+
+                if tot_signal < 0:
+                    continue  # not physical result
+
+                bin_val = np.floor((2 ** bit_depth) * tot_signal / max_signal)
+
+                bins[bin_val] += 1
+
+            else: # backscattering
+                seed = rnd.random()
+                angles = angledist[0]
+                probability = angledist[1]
+                angle = linear_interpolation(probability, angles, seed)
+
+                if angle < 110:
+                    continue # will not reach crystal
+
+                backsignal = backscatter(analogsig / gain, gain)
+                tot_signal = backsignal + noise_signal
+                if tot_signal < 0:
+                    continue  # not physical result
+                bin_val = np.floor((2 ** bit_depth) * tot_signal / max_signal)
+
+                bins[bin_val] += 1
+
+    return bins
+
+
+def mcintegral(theta_m, theta_p ,N = 100000 ):
+    int_vol = 2 * np.pi * (theta_p - theta_m)
+    int_vals = []
+
+    for i in range(N):
+        dtheta = theta_p - theta_m
+        theta = theta_m + rnd.random() * dtheta
+
+        int_vals.append(cross_diff(theta, 662)*np.sin(theta))
+
+    err = int_vol * np.std(int_vals) / np.sqrt(N)
+
+    integral_est = (int_vol / N) * sum(int_vals)
+    return integral_est, err
+
+
+integral = []
+angles = []
+values = np.arange(0,180, 0.5)
+for i in values: # cumulative distribution function made from Kein Nishina area
+    theta_m  = 0
+    theta_p = i*np.pi/180
+    integral_est, err = mcintegral(theta_m, theta_p)
+    integral.append(integral_est)
+    angles.append(i)
+
+for i in range(len(angles)):
+    if i == 0: # edge case 0 (beginning)
+        angles2 = [0]
+        errors = [0]
+        areas = [integral[i]]
+    else :
+        if i % 5 == 0: #binning values every 5 points
+            average = np.mean(integral[i-5:i])
+            error = np.std(integral[i-5:i])
+            areas.append(average)
+            errors.append(error)
+            angles2.append(angles[i-2])
+
+value180 = linear_interpolation(angles2, areas, 180)
+areas.append(value180)
+errors.append(0)
+angles2.append(180)
+
+areas = areas/areas[-1]
+errors = errors/areas[-1]
+
+plt.scatter(angles2, areas, color =  "k")
+plt.title("CDF for Kein-Nishina cross section")
+plt.xlabel("Angle (degrees")
+plt.ylabel("Probability")
+plt.show()
 
 #%%
 
-# Subtracting the scattered data from the respective background measurements
-cs20 = c20cs137-b20cs137
-cs30 = c30cs137-b30cs137
-cs45 = c45cs137-b45cs137
 
-# Calculating the transmission ratio
-r20 = []
-for i in range(len(c20cs137)):
-    if b20cs137[i] != 0:
-        value = c20cs137[i]/b20cs137[i]
-    elif b20cs137[i] == 0:
-        value = 0
-    r20.append(value)
+electron_density = 17.41e28
+scattererlength = 4e-4
+
+diameter = 15e-3 #detector diameter
+radius = 0.3 #radius at which detector is placed
+N = int(100000)
+scatter_angle = 30
+theta = scatter_angle*np.pi/180 # 10 degrees in radians
+dtheta = theta_detector(radius, diameter)
+
+sigma, sigmasigma = mcintegral(theta-dtheta/2,theta+dtheta/2 )
+
+N_angle = int(np.floor(N*(1-np.exp(-electron_density*scattererlength*sigma))))
+print(N_angle)
+
+gain1 = 2.85e-3
+gain2 = 5e-3
+
+# Experimental parameters
+
+source_energy = 662
+e_energy = 511
+
+analsig30 = analogsignal(scatter_angle, source_energy, gain1, e_energy)
 
 
-r30 = []
-for i in range(len(c30cs137)):
-    if b30cs137[i] != 0:
-        value = c30cs137[i]/b30cs137[i]
-    elif b30cs137[i] == 0:
-        value = 0
-    r30.append(value)
+botheffects0 = generate_noise(N, source_energy*gain1, gain1, [angles2, areas],probdect= 0.7, relprob= 0.8 )
 
-r45 = []
-for i in range(len(c45cs137)):
-    if b45cs137[i] != 0:
-        value = c45cs137[i]/b45cs137[i]
-    elif b45cs137[i] == 0:
-        value = 0
-    r45.append(value)
-   
-
-#expected energy responses
-expected20 = csenergy(20, cs137e[1])
-expectedchannel20 = (expected20 - popt[1])/popt[0]
-expected30 = csenergy(30, cs137e[1])
-expectedchannel30 = (expected30 - popt[1])/popt[0]
-expected45 = csenergy(45, cs137e[1])
-expectedchannel45 = (expected45 - popt[1])/popt[0]
 
 plt.figure()
-plt.scatter(channel, cs137calib)
-#plt.scatter(channel, nosource, label='background')# angle = 20 deg
-plt.axvline(cschann1,color = "r", label = ("Value from compton eq."))
-plt.title("Cs-137 at 0 degrees of incidence")
-plt.legend()
+
+plt.scatter(botheffects0.keys(), botheffects0.values(), label = "No scatter")
+plt.title("Simulated Compton edge")
 plt.xlabel("Channel")
 plt.ylabel("Counts")
 plt.grid()
 plt.show()
 
-plt.figure()
-plt.scatter(channel, r20, label='Transmission ratio')
-#plt.scatter(channel, c20cs137)
-#plt.scatter(channel, b20cs137, label='background')# angle = 20 deg
-plt.axvline(expectedchannel20,color = "r", label = ("Value from compton eq."))
-plt.title("Cs-137 at 20 degrees of incidence")
-plt.legend()
-plt.xlabel("Channel")
-plt.ylabel("Ratio")
-plt.grid()
-plt.show()
-
-plt.figure()
-plt.scatter(channel, cs30, label='scatter data - background')
-#plt.scatter(channel, c30cs137)
-#plt.scatter(channel, b30cs137  , label='background')# angle = 30 deg
-plt.axvline(expectedchannel30, color = "r", label = ("Value from compton eq."))
-plt.title("Cs-137 at 30 degrees of incidence")
-plt.legend()
-plt.xlabel("Channel")
-plt.ylabel("Total counts")
-plt.grid()
-plt.show()
-
-plt.figure()
-plt.scatter(channel, cs45, label='scatter data - background')
-#plt.scatter(channel, c45cs137)
-#plt.scatter(channel, b45cs137, label = 'background')  # angle = 40
-plt.axvline(expectedchannel45,color = "r", label = ("Value from compton eq."))
-plt.title("Cs-137 at 45 degrees of incidence")
-plt.legend()
-plt.xlabel("Channel")
-plt.ylabel("Total counts")
-plt.grid()
-plt.show()
-
-maxenergy20 = popt[0]*np.argmax(cs20) + popt[1]
-maxenergy30 = popt[0]*np.argmax(cs30) + popt[1]
-maxenergy45 = popt[0]*np.argmax(cs45) + popt[1]
-
-
 #%%
-# Investigating and simulating the difference in peak values for different scattering
-# angles    
+#relative distributions:
+
+
+electron_density = 17.41e28
+scattererlength = 4e-4
+
+diameter = 15e-3 #detector diameter
+radius = 0.3 #radius at which detector is placed
+
+angle1 = 30
+angle2 = 20
+
+theta1 = angle1*np.pi/180 # 10 degrees in radians
+dtheta1 = theta_detector(radius, diameter)
+sigma1, sigmasigma1 = mcintegral(theta-dtheta/2,theta+dtheta/2 )
+
+theta2 = angle2*np.pi/180 # 10 degrees in radians
+dtheta2 = theta_detector(radius, diameter)
+sigma2, sigmasigma2 = mcintegral(theta-dtheta/2,theta+dtheta/2 )
+
+n1 = 10000
+n2 = int(np.floor(np.exp(electron_density*scattererlength*(sigma2 - sigma1))*n1))
+
+analsig1 = analogsignal(angle1, source_energy, gain1, e_energy)
+analsig2 = analogsignal(angle2, source_energy, gain1, e_energy)
+
+spectrum1 = generate_noise(n1, analsig1, gain1)
+spectrum2 = generate_noise(n2, analsig2, gain1)
 
 plt.figure()
-plt.scatter(channel, cs30, label='30 degree scattering angle')
-#plt.scatter(channel, c30cs137)
-#plt.scatter(channel, b30cs137  , label='background')# angle = 30 deg
-plt.title("Cs-137 at 30 degrees of incidence")
-
-
-plt.scatter(channel, cs20, color = 'r', label='20 degree scattering angle')
-#plt.scatter(channel, c45cs137)
-#plt.scatter(channel, b45cs137, label = 'background')  # angle = 40
-plt.title("Cs-137 at 20 degrees of incidence")
-plt.legend()
-plt.xlabel("Channel")
-plt.ylabel("Total counts")
+plt.scatter(spectrum1.keys(), spectrum1.values(), label = str(angle1) + " degrees")
+plt.scatter(spectrum2.keys(), spectrum2.values(), label = str(angle2) + " degrees")
 plt.grid()
-
-import MC as mc
-
-
-
-
-    
-
-
-
-
+plt.legend()
+plt.title("Checking relative count rates")
+plt.show()
